7d2d9ff206445d1ac36bcada280a8f46e738845f
Riko <riko.fluchel@gmail.com>
2025-04-07 10:17:52 -0400

    add Directors page

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -2,13 +2,39 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState(null)
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then(r => r.json())
+      .then(data => setDirectors(data))
+      .catch(error => console.error(error))
+  }, []);
+
+
   return (
     <>
       <header>
         <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors ? (
+          <>
+          {directors.map((director, index) => (
+            <article key={index}>
+              <h2>{director.name}</h2>
+              <ul>
+                {director.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))}
+              </ul>
+            </article>
+          ))}
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </main>
     </>
   );
