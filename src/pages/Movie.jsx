fc2a80fd8526c827e7746d8e76a08b6c4624f73d
Riko <riko.fluchel@gmail.com>
2025-04-07 10:06:16 -0400

    add initial render of movie cards and routing to movie page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then(r => r.json())
+      .then(data => setMovie(data))
+      .catch(error => console.error(error))
+  }, [movieId])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        {movie ? (
+          <>
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+            <span key={index} style={{ marginRight: '8px' }}>{genre}</span>
+          ))}
+        </div>
+        </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </main>
     </>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,25 @@
-
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/movie";
+import Home from "./pages/Home";
 
 const routes = [
-   
+   {
+    path: "/",
+    element: <Home />,
+   },
+   {
+    path: "/directors",
+    element: <Directors />,
+   },
+   {
+    path: "/actors",
+    element: <Actors />
+   },
+   {
+    path: "/movie/:id",
+    element: <Movie />
+   }
   ];
 
 export default routes;
