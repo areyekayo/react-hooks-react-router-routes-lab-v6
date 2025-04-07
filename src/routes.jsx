e6caf87d1f4552348fc3860d3162936eb4a207bc
Riko <riko.fluchel@gmail.com>
2025-04-07 10:31:55 -0400

    add ErrorPage to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,23 +2,28 @@ import Directors from "./pages/Directors";
 import Actors from "./pages/Actors";
 import Movie from "./pages/movie";
 import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
    {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />
    },
    {
     path: "/directors",
     element: <Directors />,
+    errorElement: <ErrorPage />
    },
    {
     path: "/actors",
-    element: <Actors />
+    element: <Actors />,
+    errorElement: <ErrorPage />
    },
    {
     path: "/movie/:id",
-    element: <Movie />
+    element: <Movie />,
+    errorElement: <ErrorPage />
    }
   ];
 
